3ea9d00eb0f40217ce430f5036984254f2a519ae
black-hole-fear <macSmith0227@outlook.com>
2024-03-01 08:05:35 +0100

    fixed initial console bugs

--- content.js
+++ content.js
@@ -1,6 +1,1 @@
-window.addEventListener('beforeunload', function(event) {
-    // Inform the background script that the page is being closed
-    console.log("hello")
-    chrome.runtime.sendMessage({ type: 'pageClosing' });
-  });
-  
+console.log("Appended content.js!");
